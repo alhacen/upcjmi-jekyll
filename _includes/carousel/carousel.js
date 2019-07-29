54938f395ccdde9d83899d3cb67fc07ac8f87fd3
Faisal Manzer <faisal_manzer@yahoo.in>
2019-07-29 13:46:28 +0530

    Changed Contact

--- _includes/carousel/carousel.js
+++ _includes/carousel/carousel.js
@@ -4,7 +4,27 @@ document.addEventListener('DOMContentLoaded', function () {
         fullWidth: true,
         duration: 300
     });
+    var models = document.querySelectorAll('.modal');
+    var modelsInstances = M.Modal.init(models);
+
     setInterval(function () {
         M.Carousel.getInstance(document.querySelector('.carousel')).next();
     }, 5000);
+
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbwRj5x_BwJNCSTlSIxhPhDYEobDjdewtMsvJR3Y6PY3CQ99IrEH/exec';
+    const form = document.forms['contact-sheet-form']
+
+    form.addEventListener('submit', e => {
+        e.preventDefault()
+        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+          .then(response => {
+              console.log('Success!', response);
+              M.toast({html: 'Thankyou for contacting us'})
+            })
+          .catch(error => {
+              console.error('Error!', error.message);
+              M.toast({html: 'Unknown error occured'});
+
+            })
+      })
 });
